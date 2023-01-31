eddbc0bdfc684e46d6243003990a1b0bd06f6cc5
cedric-joan <95696368+cedric-joan@users.noreply.github.com>
2023-01-31 22:35:49 +0100

    Récupération des films depuis l'API

---
+++ front-end/js/dom/indexFonction.js
@@ -0,0 +1,67 @@
+// création d'un lien
+function createLink(id) {
+  const link = document.createElement("a");
+  link.href = `http://localhost:8000/api/v1/titles/${id}`;
+  return link;
+}
+// afficher le nom du produit
+function createTitle(name) {
+  const items = document.querySelector("#movieItems");
+  const title = document.createElement("h2");
+  title.classList.add("title");
+  title.innerText = name;
+  items.appendChild(title);
+  return items;
+}
+// afficher une description du produit
+function createParagraph(description) {
+  const items = document.querySelector("#movieItems");
+  const paragraph = document.createElement("p");
+  paragraph.classList.add("description");
+  items.appendChild(paragraph);
+  paragraph.innerText = description;
+}
+// créer l'élément image du produit
+function createImage(image_url) {
+  const items = document.querySelector("#bestMovieItems");
+  const image = document.createElement("img");
+  image.src = image_url;
+  items.appendChild(image);
+}
+function makeBestmoviesScoresImages(id, image_url){
+  const link = document.createElement("a");
+  link.href = `http://localhost:8000/api/v1/titles/${id}`;
+  const items = document.querySelector("#bestScoresItems");
+  const image = document.createElement("img");
+  image.src = image_url;
+  link.appendChild(image)
+  items.appendChild(link);
+}
+function makeBestmoviesActionImages(id, image_url){
+  const link = document.createElement("a");
+  link.href = `http://localhost:8000/api/v1/titles/${id}`;
+  const items = document.querySelector("#actionItems");
+  const image = document.createElement("img");
+  image.src = image_url;
+  link.appendChild(image);
+  items.appendChild(link)
+}
+function makeBestmoviesAdventureImages(id, image_url){
+  const link = document.createElement("a");
+  link.href = `http://localhost:8000/api/v1/titles/${id}`;
+  const items = document.querySelector("#fantasyItems");
+  const image = document.createElement("img");
+  image.src = image_url;
+  link.appendChild(image);
+  items.appendChild(link)
+}
+function makeBestmoviesComedyImages(id, image_url){
+  const link = document.createElement("a");
+  link.href = `http://localhost:8000/api/v1/titles/${id}`;
+  const items = document.querySelector("#comedyItems");
+  const image = document.createElement("img");
+  image.src = image_url;
+  link.appendChild(image);
+  items.appendChild(link)
+}
+export {makeBestmoviesComedyImages, makeBestmoviesAdventureImages, makeBestmoviesActionImages, makeBestmoviesScoresImages, createImage, createLink, createParagraph, createTitle };
---
+++ front-end/js/script.js
@@ -0,0 +1,93 @@
+// import des fonctions dans indexFonctions
+import {
+  createLink,
+  createImage,
+  createTitle,
+  createParagraph,
+  makeBestmoviesScoresImages,
+  makeBestmoviesActionImages,
+  makeBestmoviesAdventureImages,
+  makeBestmoviesComedyImages,
+} from "./dom/indexFonction.js";
+// récupérer le film avec le meilleur score du server
+const urlApi = "http://localhost:8000/api/v1/titles/9008642";
+fetch(urlApi)
+  .then((res) => res.json())
+  .then((data) => displayMovies(data))
+  .catch((err) => console.error(err));
+
+// récupérer les éléments du film
+function displayMovies(res) {
+  const data = res;
+  const { id, title, description, image_url } = data;
+  createLink(id);
+  createImage(image_url);
+  createTitle(title);
+  createParagraph(description);
+}
+
+// récupérer tous les films les mieux notés toutes catégories du server
+for (let i = 1; i < 3; i++) {
+  const urlApi = `http://localhost:8000/api/v1/titles/?imdb_score_min=9.4&page=${i}`;
+  fetch(urlApi)
+    .then((res) => res.json())
+    .then((data) => displayBestmoviesScores(data))
+    .catch((err) => console.error(err));
+}
+
+function displayBestmoviesScores(res) {
+  const data = res.results;
+  for (let i = 0; i < 5; i++) {
+    const { id, image_url } = data[i];
+    makeBestmoviesScoresImages(id, image_url);
+  }
+}
+// récupérer tous les films les mieux notés catégorie action du server
+for (let i = 2; i < 4; i++) {
+  const urlApi = `http://localhost:8000/api/v1/titles/?genre=action&imdb_score_min=8.8&page=${i}`;
+  fetch(urlApi)
+    .then((res) => res.json())
+    .then((data) => displayBestmoviesAction(data))
+    .catch((err) => console.error(err));
+}
+
+function displayBestmoviesAction(res) {
+  const data = res.results;
+  for (let i = 1; i < 5; i++) {
+    const { id, image_url } = data[i];
+    makeBestmoviesActionImages(id, image_url);
+    console.log(data);
+  }
+}
+// récupérer tous les films les mieux notés catégorie aventure du server
+for (let i = 1; i < 3; i++) {
+  const urlApi = `http://localhost:8000/api/v1/titles/?genre=adventure&imdb_score_min=8.8&page=${i}`;
+  fetch(urlApi)
+    .then((res) => res.json())
+    .then((data) => displayBestmoviesAdventure(data))
+    .catch((err) => console.error(err));
+}
+
+function displayBestmoviesAdventure(res) {
+  const data = res.results;
+  for (let i = 0; i < 4; i++) {
+    const { id, image_url } = data[i];
+    makeBestmoviesAdventureImages(id, image_url);
+  }
+}
+// récupérer tous les films les mieux notés catégorie comedy du server
+for (let i = 1; i < 3; i++) {
+  const urlApi = `http://localhost:8000/api/v1/titles/?genre=comedy&imdb_score_min=9.1&page=${i}`;
+  fetch(urlApi)
+    .then((res) => res.json())
+    .then((data) => displayBestmoviesComedy(data))
+    .catch((err) => console.error(err));
+}
+
+function displayBestmoviesComedy(res) {
+  const data = res.results;
+  for (let i = 0; i < 5; i++) {
+    const { id, image_url } = data[i];
+    makeBestmoviesComedyImages(id, image_url);
+  }
+}
